ab6eb4f649592e9703aca048236fa78cfb91d4f3
user-ls17 <atze3011@web.de>
2017-05-28 19:33:47 +0200

    Add files via upload

---
+++ ServerApplication/src/Client.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String[] args) {
+		try {
+			Socket client = new Socket("localhost",5555);
+			System.out.println("Client gestartet");
+			
+			//Streams
+			OutputStream out = client.getOutputStream();
+			PrintWriter writer = new PrintWriter(out);
+			
+			InputStream in = client.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			//----------------------------			
+			
+			writer.write("Hallo Server!");
+			writer.flush();
+			
+			writer.close();
+			reader.close();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ServerApplication/src/Server.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(5555);
+			System.out.println("Server gestartet");
+			
+			Socket client = server.accept();
+			
+			//Streams
+			
+			OutputStream out = client.getOutputStream();
+			PrintWriter writer = new PrintWriter(out);
+			
+			InputStream in = client.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			//----------------------------			
+			
+			String s = null;
+			
+			while((s=reader.readLine()) != null )
+			{
+				System.out.println("Empfangen vom Client:" + s);
+			}
+			
+			writer.close();
+			reader.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
